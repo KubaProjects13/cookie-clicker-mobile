2defbdd354a06755c9a616c86d13c2df36ce622a
KubaProjects13 <kubin.ma.email@gmail.com>
2026-02-08 20:33:41 +0100

    Update shop.js

--- clicker arena/js/menu/shop.js
+++ clicker arena/js/menu/shop.js
@@ -9,51 +9,68 @@ const buybutton = SkinOffer.querySelector("button");
 const gemsLabels = document.querySelectorAll(".gemsLabel");
 
 function updateGemLabels() {
-  gemsLabels.forEach(label => {
-    label.textContent = gems + " 💎";
-  });
+    gemsLabels.forEach(label => {
+        label.textContent = gems + " 💎";
+    });
 };
 
 function updateSkinCollection() {
 
-  if (SkinCollection.includes("1") || SkinCollection.includes("2") && !SkinCollection.includes("3")) {
-    h2.textContent = "white chocolate";
-    p.textContent = "Tasty!!";
-    img.src = "img/white/cookie 300x300.png";
-    span.textContent = "price: 1000 gems";
-
-    buybutton.onclick = () => {
-      if (gems >= 1000) {
-        localStorage.setItem("skin-collection", "13");
-        gems = gems - 1000
-        updatestorage();
-        updateGemLabels();
-      } else {
-        alert("You don't have enough gems!");
-      }
-    };
-
-  } else if (SkinCollection.includes("3") && !SkinCollection.includes("2")) {
+  // má default, nemá dark ani white → nabídni dark
+  if (
+    SkinCollection.includes("1") &&
+    !SkinCollection.includes("2") &&
+    !SkinCollection.includes("3")
+  ) {
     h2.textContent = "dark chocolate";
-    p.textContent = "Tasty!!";
+    p.textContent = "tastes good";
     img.src = "img/dark/cookie 300x300.png";
-    span.textContent = "price: 500 gems";
+    span.textContent = "price: 500💎";
 
     buybutton.onclick = () => {
       if (gems >= 500) {
-        localStorage.setItem("skin-collection", "123");
-        gems = gems - 500
+        SkinCollection = "12";
+        gems -= 500;
         updatestorage();
         updateGemLabels();
+
+        location.reload();
       } else {
         alert("You don't have enough gems!");
       }
     };
+  // má dark, nemá white → nabídni white
+  } else if (
+    SkinCollection.includes("2") &&
+    !SkinCollection.includes("3")
+  ) {
+    h2.textContent = "white chocolate";
+    p.textContent = "so white";
+    img.src = "img/white/cookie 300x300.png";
+    span.textContent = "price: 1000💎";
+
+    buybutton.onclick = () => {
+      if (gems >= 1000) {
+        SkinCollection = "123";
+        gems -= 1000;
+        updatestorage();
+        updateGemLabels();
+
+        location.reload();
+      } else {
+        alert("You don't have enough gems!");
+      }
+    };
+  // má všechno
   } else {
     h2.textContent = "you have all skins!";
     p.textContent = "wow!!";
     img.src = "img/ui/cookies.png";
     span.textContent = "";
+    buybutton.style.color = "#a9a9a9";
+    buybutton.style.backgroundColor = "#614810";
+    buybutton.style.boxShadow = "0 4px 0 #47350d";
+    buybutton.style.cursor = "not-allowed";
   }
 }
 
