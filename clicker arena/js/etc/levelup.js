c2cfa757731ee006c6637d127352418c4d159b25
KubaProjects13 <kubin.ma.email@gmail.com>
2026-02-09 15:55:03 +0100

    Delete clicker arena directory

--- clicker arena/js/etc/levelup.js
+++
@@ -1,9 +0,0 @@
-const fromlevellabel = document.getElementById("fromlevelinfo");
-const gotItButton = document.getElementById("gotitButton");
-const rewardsLabel = document.getElementById("rewards");
-
-fromlevellabel.textContent = "level " + level;
-
-gotItButton.addEventListener("click", () => {
-    window.location.href = "../index.html"
-});
