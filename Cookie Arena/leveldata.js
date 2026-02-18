bbff4d4ac199994048dd26146ce50e0a5dd1fcf8
KubaProjects13 <kubin.ma.email@gmail.com>
2026-02-18 19:15:33 +0100

    Delete Cookie Arena directory

--- Cookie Arena/leveldata.js
+++
@@ -1,9 +0,0 @@
-const levels = {
-    2: { required: 100, timeMinus: 2, powerPlus: 0.5, gemsPlus: 10 },
-    3: { required: 200, timeMinus: 2, powerPlus: 0, gemsPlus: 10 },
-    4: { required: 400, timeMinus: 0, powerPlus: 0, gemsPlus: 20 },
-    5: { required: 600, timeMinus: 1, powerPlus: 0.5, gemsPlus: 20 },
-    6: { required: 800, timeMinus: 1, powerPlus: 0, gemsPlus: 20 },
-    7: { required: 1000, timeMinus: 2, powerPlus: 0, gemsPlus: 20 },
-    8: { required: 1400, timeMinus: 0, powerPlus: 0.5, gemsPlus: 30 },
-};
